455a29a811a7557160538c25a24aab874043e6d0
Neutron17 <sandormatyas17@gmail.com>
2022-03-05 17:30:38 +0100

    1.1

--- src/main.c
+++ src/main.c
@@ -15,79 +15,100 @@
 #include <getopt.h>	// getopt	(parseArgs)
 #include "stdext.h"	// itoa		(main)
 
-/* dps: depends */
+#define CMD_SZ 7
 
-typedef void (*CmdFn)(unsigned*);
+void inc(unsigned *n, int arg);
+void dec(unsigned *n, int arg);
+void shl(unsigned *n, int arg);
+void shr(unsigned *n, int arg);
+void new(unsigned *n, int arg);
+void add(unsigned *n, int arg);
+void rem(unsigned *n, int arg);
 
-#define BIN(NUM, DEST) itoa(NUM, DEST, 2)
-#define OCT(NUM, DEST) itoa(NUM, DEST, 6)
-#define DEC(NUM, DEST) itoa(NUM, DEST, 10)
-#define HEX(NUM, DEST) itoa(NUM, DEST, 16)
+typedef void (*CmdFn)(unsigned*, int);
 
-#define VERSION 1.0
-#define CMD_SZ  4
-
-bool isDebug = false;
-bool useLast = true;
-//bool running = true;
-char numStr[7];
-const char *commands[CMD_SZ] = {
-	"inc",
-	"dec",
-	"shl",
-	"shr"
-};
-void parseArgs(int argc, char *argv[]);
-bool isInLUT(const char *command);
-
-void inc(unsigned *n);
-void dec(unsigned *n);
-void shl(unsigned *n);
-void shr(unsigned *n);
-
-CmdFn cmdFns[CMD_SZ] = {
+const CmdFn cmdFns[CMD_SZ] = {
 	inc,
 	dec,
 	shl,
-	shr
+	shr,
+	new,
+	add,
+	rem
 };
+const char *commands[CMD_SZ] = {
+	"inc",	// increment
+	"dcr",	// decrement
+	"shl",	// shift left
+	"shr",	// shift right
+	"new",	// new number
+	"add",	// add %n%
+	"rem"	// min %n%
+};
+enum CmdEnum {
+	INC,
+	DEC,
+	SHL,
+	SHR,
+	NEW,
+	ADD,
+	REM
+};
+
+#define VERSION 1.1
+
+bool isDebug = false;
+bool useLast = true;
+char numStr[7];
+unsigned num;
+
+void parseArgs(int argc, char *argv[]);
+bool isInLUT(const char *command);
 
 int main(int argc, char *argv[]) {
 	srand(time(NULL));	// seed
 	parseArgs(argc, argv);	// dps: isDebug, useLast, numStr 
         char buffer[9];
-        unsigned x;
 	if(useLast)
 		strcpy(numStr, argv[argc-1]);
-	x = atoi(numStr);
-	if(x == 0) { // atoi failed
-		x = rand() % UINT_MAX;
-		printf("No number set, using random: %u\n", x);
+	num = atoi(numStr);
+	if(num == 0) { // atoi failed
+		num = rand() % UINT_MAX;
+		printf("No number set, using random: %u\n", num);
 	}
-        unsigned char *ptr = (unsigned char *)&x;
+        unsigned char *ptr = (unsigned char *)&num;
         char *str = (char *)malloc(9);
         strcpy(str, "00000000");
 	char *cmd = (char *) malloc(64 * sizeof(char));
+	int arg = -0;
  	while(true) {
-		ptr = (unsigned char *)&x;
+		ptr = (unsigned char *)&num;
 inchk:
-		scanf("%64s", cmd);
+		scanf("%64s %d", cmd, &arg);
 		if(strcmp(cmd, "q") == 0) {
 			printf("Exiting\n");
 			goto exit;
 		}
+		for(int i = 0; i < CMD_SZ; i++) {
+			if(strcmp(commands[i], cmd) == 0) {
+				printf("%s\n", commands[i]);
+				cmdFns[i](&num, arg);
+			}
+		}
 		if(!isInLUT(cmd)) {
+			if(isDebug)
+				printf("Command: %s\n", cmd);
 			printf("Not a command\n");
 			goto inchk;
 		}
 		
-		for(int i = 0; i<sizeof(x); i++) {
+		for(int i = 0; i<sizeof(num); i++) {
                 	BIN(*ptr, buffer);
         	        strcpy(str+8-strlen(buffer), buffer);
 			if(isDebug) {
                 		printf(
-                		"i:%d str:%s buffer:%s x:%u ptr:%p\n"
-                		,i,str,buffer,x,(void *)ptr-(void *)&x);
+                		"i:%d str:%s buffer:%s num:%u ptr:%p\n"
+                		,i,str,buffer,num,(void *)ptr-(void *)&num);
 			}
                 	printf("\e[41m%s\e[0m\n", str);
         	        strcpy(str, "00000000");
@@ -101,17 +122,26 @@ exit:
 	return EXIT_SUCCESS;
 }
 
-void inc(unsigned *n) {
+void inc(unsigned *n, int arg) {
 	*n = *n + 1;
 }
-void dec(unsigned *n) {
+void dec(unsigned *n, int arg) {
 	*n = *n - 1;
 }
-void shl(unsigned *n) {
+void shl(unsigned *n, int arg) {
 	*n = *n << 1;
 }
-void shr(unsigned *n) {
+void shr(unsigned *n, int arg) {
 	*n = *n >> 1;
+}
+void new(unsigned *n, int arg) {
+	num = arg;
+}
+void add(unsigned *n, int arg) {
+	*n += arg;
+}
+void rem(unsigned *n, int arg) {
+	*n -= arg;
 }
 
 // TODO faster search algorithm
--- src/stdext.h
+++ src/stdext.h
@@ -1,6 +1,11 @@
 #ifndef _NTR_STDEXT_H_
 #define _NTR_STDEXT_H_ 1
 
+#define BIN(NUM, DEST) itoa(NUM, DEST, 2)
+#define OCT(NUM, DEST) itoa(NUM, DEST, 6)
+#define DEC(NUM, DEST) itoa(NUM, DEST, 10)
+#define HEX(NUM, DEST) itoa(NUM, DEST, 16)
+
 void strreverse(char* begin, char* end);
 void itoa(int value, char* str, int base);
 
